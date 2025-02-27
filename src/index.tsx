aafa5a042377d9d66ccd0164265e8c56bf2e2c8b
Carlos A. <carlosvsk@gmail.com>
2025-02-27 16:18:09 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -70,31 +70,6 @@ stls.height({ pass: { arrayValue: [jsvals.varReader({pass: {
               }
               ],
 
-          childrenItems: [()=><></>],
-
-          pressableFunctions: [
-        (...args) => {
-          // ---------- get Function from A_Project Scope
-          return tools.goTo("sc1");
-        }
-        ],
-
-          args,
-        }}/>, 
-        (...args:any) => <Elements.Pressable3 pass={{
-          elementProperties: [{}],
-
-          styles: [
-              {
-                backgroundColor: 'blue',
-                borderRadius: 20,
-                paddingVertical: 5,
-                paddingHorizontal: 20,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }
-              ],
-
           childrenItems: [(...args:any) => <Elements.Text pass={{
           arrProps: [
             {}
